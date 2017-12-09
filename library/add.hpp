ea5bf50d26167aa88e1f806fb3d0cce4a9435fd7
hanan6 <hanan.najim6@gmail.com>
2017-12-09 15:37:46 +0100

    add add function and the particular case of stack registers

--- library/add.hpp
+++ library/add.hpp
@@ -13,97 +13,138 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
  * -------------------------------------------------------------------------- */
-
+ 
  /*! \file add.hpp
  *
- *  \author Adnan44 <adnane.mounassib@gmail.com>
+ *  \author Hanan6 <hanan.najim6@gmail.com>
  *  \version 1.0
  *  \date october 2017
  */
 
 
-#ifndef __ASMX__Add__
-#define __ASMX__Add__
+#ifndef __ASMX__ADD__
+#define __ASMX__ADD__
 
 /* --------------------------------------------------------------------------
  *  Modules
  * -------------------------------------------------------------------------- */
 
+#include <string>
+#include <vector>
 #include "parser/utils.hpp"
 
-#include "engine/stack.hpp"
-#include "engine/register.hpp"
-#include "engine/variable.hpp"
+
+
 
 /* --------------------------------------------------------------------------
  *  Functions
  * -------------------------------------------------------------------------- */
 
-
 class AsmAdd: public AsmRegisterCollection,AsmVariableCollection , AsmStack
 {
-
-
+    
+   private:
+    
+     AsmStack * __stack;
     public:
+    
+ 
         /*! Constructor
          *
-         *  \param name the function name
+         *  \param reg the Registers Collection
+         *  \param var the variables Collection
+         *  \param stack
+         *  
          */
-    AsmAdd(vector<AsmRegister*> reg,vector<AsmVariable*> var):
-        AsmRegisterCollection(reg),AsmVariableCollection(var),AsmStack()
+        AsmAdd(vector<AsmRegister*> reg,vector<AsmVariable*> var,AsmStack * stack):
+        AsmRegisterCollection(reg),AsmVariableCollection(var),__stack(stack)
         {}
-
-        /*! function add
+           
+        
+        
+        /*! Add function 
          *
-         *  \add function for add instruction :
-         *  \add destination, source
-         *  \destination = destination + source
+         *  \param destination
+         *  \param source
+         *  
          */
-        void add(std::string destination, std::string source)
+        void add(string destination,string source)
         {
-        if(ifMemory(destination)){
-            int size = extractSize(destination);
-            int dest = AsmStack::get_value(size);
-            if(ifInt(source)){
-                int src = std::stoi(source);
-                dest += src;
-                AsmStack::push(dest,size);
+              
+                int src = get_value(source);
+                int dest= get_value(destination);
+                dest=dest+src;
+            
+            if(ifRegister(destination)) 
+            {
+                /* the dastination is a stack register 
+                   add esp,16 = remove the first 16 bits in the stack */
+                   
+                if(findRegister(destination)->get_label().find("sp") != std::string::npos)
+                {
+                     __stack->remove(src);   
+                
+                }
+                else
+                {   
+                    findRegister(destination)->set_value(dest);
+                                
+                }
+
             }
-            if(ifRegister(source)){
-                int src = findRegister(source)->get_value();
-                int size2 = findRegister(source)->get_size();
-                dest += src;
-                AsmStack::push(dest, size2);
-            }
-            if(ifMemory(source)){
-                int size2 = extractSize(source);
-                int src = AsmStack::get_value(size2);
-                dest += src;
-                AsmStack::push(dest, size2);
+                
+                
+            if(ifMemory(destination))
+            {
+                int size = extractSize(destination);
+                 __stack->set_value(size,dest);
             }
         }
-        if (ifRegister(destination)){
-            int dest = findRegister(destination)->get_value();
-            if(ifInt(source)){
-                int src = toInt(source);
-                dest += src;
-                findRegister(destination)->set_value(dest);
-            }
-            if(ifRegister(source)){
-                int src = findRegister(source)->get_value();
-                dest += src;
-                findRegister(destination)->set_value(dest);
-            }
-            if(ifMemory(source)){
-                int size = extractSize(source);
-                int src = AsmStack::get_value(size);
-                dest += src;
-                findRegister(destination)->set_value(dest);
-            }
-
-            }
-
+        
+        /* Return the real value of the parameter 
+         *
+         *  \param parameter parameter to evaluate 
+         *  \note get value from  register or  stack
+         *  \note if the parameter is variable get the id that reference to the variable address in
+         *   the stack 
+         *  \return int value
+         *  
+         */
+        
+        
+        int get_value(std::string parameter)
+        {
+         
+         if(ifRegister(parameter))
+         {
+           
+            if(findRegister(parameter) != NULL) 
+                return  findRegister(parameter)->get_value(); 
+      
+            else
+			    return findVariable(parameter)->get_id() ;
+          } 
+          			  
+	       if(ifInt(parameter))
+	        return std::stoi(parameter);
+	        
+	       	if(ifMemory(parameter)){
+	       	
+	       	    if (ifStack(parameter)) 
+	       	    {
+			         
+			         int size = extractSize(parameter);
+				     return __stack->get_value(size); 
+			    }
+			    else
+			    {		
+				     parameter = extractVariable(parameter);
+				     return std::stoi(findVariable(parameter)->get_value()) ;	
+			   	}		
+			}				        		  			
+	       return 0;				
+        }       
 
 };
+#endif
 
-#endif // __ASMX__Add__
